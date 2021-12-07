5baadec3e8fa9ae14652d05eb42c453f8ec24951
Letisouza <leticiasouzaufpe@gmail.com>
2021-12-06 22:01:17 -0300

    Caminho das pedras

--- R/criando blog.R
+++ R/criando blog.R
@@ -10,3 +10,12 @@ usethis::use_git()
 # Pushing
 usethis::use_github()
 
+# Github Page: informando que o que tiver no repositório seja transmitido num site.
+usethis::use_github_pages("master", "/docs")
+
+# Gerando o blog
+file.create(".nojekyll")
+distill::create_blog(".", "Leticia de Souza", gh_pages = T)
+
+# Criando posts: gera um Rmd para edição de textos
+distill::create_post("Hello, world")
